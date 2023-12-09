28c9fa481c844f66d065d896d67e1a897b2e16d0
Tonny Lang'o <tonnylango@gmail.com>
2023-12-09 16:15:47 +0300

    Default commit message

--- README.md
+++ README.md
@@ -1,8 +1,5 @@
-# React + Vite
-
-This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
-
-Currently, two official plugins are available:
-
-- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
-- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+a tic-tac-toe game that:
+1. Lets you Play tic-tac-toe
+2. Indicates when a player has won the game
+3. Stores a game's history as a game progresses
+4. Allows players to review a games's history and see previous versions of a games board
--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,50 @@ import { useState } from "react";
 //TODO allow multiple boards
 //TODO add a timer
 
+// TODO: For the current move only, show “You are at move #…” instead of a button.
+
+// TODO: Rewrite Board to use two loops to make the squares instead of hardcoding them.
+
+// TODO: Add a toggle button that lets you sort the moves in either ascending or descending order.
+
+// TODO: When someone wins, highlight the three squares that caused the win (and when no one wins, display a message about the result being a draw).
+
+// TODO: Display the location for each move in the format (row, col) in the move history list.
+
+
+export default function Game() {
+  const [history, setHistory] = useState([Array(9).fill(null)]);
+  const [currentMove, setCurrentMove] = useState(0)
+  const xIsNext = currentMove % 2 === 0
+  const currentSquares = history[currentMove];
+
+  function handlePlay(nextSquares){
+    const nextHistory = [...history.slice(0, currentMove + 1), nextSquares]
+    setHistory(nextHistory) //FIXME go the way leetcode showed you to update the history array
+    setCurrentMove(nextHistory.length - 1)
+  }
+
+  function jumpTo(nextMove){
+    setCurrentMove(nextMove)
+  }
+
+  const moves = history.map((square, move) => {
+    let description = move > 0 ? 'Go to move #' + move : 'Go to game start';
+    return <li key={move}><button onClick={() => jumpTo(move)}>{description}</button></li>
+  })
+
+  //TODO make the div not squish into each other, move buttons down
+  return ( 
+  <div className="game">
+    <div className="game-board">
+      <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
+    </div>
+    <div className="game-info">
+      <ol>{moves}</ol>
+    </div>
+  </div>
+  )
+}
 
 function Square({value, onSquareClick}) {
   
@@ -14,10 +58,7 @@ function Square({value, onSquareClick}) {
   return <button className="square" onClick={onSquareClick}>{value}</button>;
 }
 
-export default function Board() {
-const [xIsNext, setXIsNext] = useState(true)
-const [squares, setSquare] = useState(Array(9).fill(null))
-
+function Board({xIsNext, squares, onPlay}) {
 function handleClick (i) {
   if (squares[i] || calculateWinner(squares)) {
     return;
@@ -25,8 +66,7 @@ function handleClick (i) {
   
   const nextSquares = squares.slice()
   nextSquares[i] = xIsNext ? "X" : "O"
-  setSquare(nextSquares)
-  setXIsNext(!xIsNext)
+  onPlay(nextSquares)
 }
 
 const winner = calculateWinner(squares)
